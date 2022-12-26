7798d7467334a42cb5c9a56f9e5f91b66abe3d42
Barny Victor <victorbarny4@gmail.com>
2022-12-26 11:44:43 +0100

    finished working on the error handler

---
+++ src/Error-handler/error-code.ts
@@ -0,0 +1,7 @@
+export class ErrorCode {
+  public static readonly Unauthenticated = "Unauthenticated";
+  public static readonly NotFound = "NotFound";
+  public static readonly NO_CONTENT = "NO_CONTENT";
+  public static readonly AsyncError = "AsyncError";
+  public static readonly INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+}
