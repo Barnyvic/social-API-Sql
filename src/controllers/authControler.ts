1bf8b4872dc12586743041d8f8df863435c406ba
Barny Victor <victorbarny4@gmail.com>
2022-12-26 12:05:07 +0100

    started working on the auth api

---
+++ src/controllers/authControler.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import { IUser } from "../utils/interface";
+import { generateToken } from "../utils/generateToken";
+
+//@desc Register new user
+//@route POST /register
+//@access Public
+
+export const createNewUser = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
